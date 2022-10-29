ff300914ff6cae00f57481064b90451e46e7b0ca
Sarvani Kandukuri <sarvanik60@gmail.com>
2022-10-29 22:38:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    colors:{
+      red:'#C21010',
+      gray:'#1B2631',
+      green:'#28B463',
+      darkGreen:'#196F3D',
+      white:'#fff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
